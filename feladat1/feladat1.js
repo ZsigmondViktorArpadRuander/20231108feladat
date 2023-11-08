5da19e929162ffdfe84b18a9020913d61b323cc7
zsigmondviktor99 <zsigmond.viktor.arpad@ruander.hu>
2023-11-08 19:37:32 +0100

    init commit

---
+++ feladat1/feladat1.js
@@ -0,0 +1,37 @@
+/*
+TYPESCRIPT
+
+- Keszits alprogramot, ami...
+    - Eldonti, hogy a parameterul kapott szam tombben van-e negativ szam
+    - Egy parametrul kapott sugarbol megallapitja a kor keruletet es teruletet
+        - Tuple-t alkalmazz
+        - Kerulet: 2 * sugar * PI
+        - Terulet: sugar * sugar * PI
+    
+- Keszits interface-t Auto neven
+    - Gyarto (szoveg)
+    - Tipus (szoveg)
+    - Hengerurtartalom (szam)
+    - BenzinesE (logikai)
+*/
+function VanENegativ(szamok) {
+    var i = 0;
+    while (i < szamok.length && !(szamok[i] < 0)) {
+        i++;
+    }
+    /*if(i < szamok.length){
+        return true;
+    }
+    else{
+        return false;
+    }*/
+    return i < szamok.length;
+}
+function KorKerTer(r) {
+    var ker = 2 * r * Math.PI;
+    var ter = r * r * Math.PI;
+    return [ker, ter];
+}
+console.log(VanENegativ([5, 12, 23, -5, 14]));
+console.log(VanENegativ([5, 12, 23, 14]));
+console.log(KorKerTer(4));
